9208c80bb8cb2b80df4773274189f1c27f5d6ad5
kikeprime <hermandavidtamas@gmail.com>
2024-10-12 20:04:45 +0200

    Creating main.py for tests and setting up death

--- wolfsheep/agent.py
+++ wolfsheep/agent.py
@@ -7,26 +7,60 @@ class WolfSheepAgent(Agent):
     def __init__(self, unique_id: int, model: WolfSheepModel, energy: int, gender: bool = False):
         super().__init__(unique_id, model)
 
+        self.energy = energy
         # True: female
         # False: male
         self.gender = gender
-        self.energy = energy
+        # The child classes set this
+        # 0: Wolf
+        # 1: Sheep
+        # 2: Grass
+        self.race = None
 
     def step(self):
+        self.model: WolfSheepModel
         self.move()
+        # Sheep don't eat and lose energy in the Wolves Sheep model
+        if not (self.model.model_type == 2 and self.race == 1):
+            self.energy -= 1
+            self.eat()
+            self.die()
 
     def move(self):
+        self.model: WolfSheepModel
+        cells_to_move = self.model.grid.get_neighborhood(
+            pos=self.pos,
+            moore=True,
+            include_center=False,
+            radius=4
+        )
+        dest_cell = self.model.random.choice(cells_to_move)
+        self.model.grid.move_agent(self, dest_cell)
+
+    def eat(self):
         pass
+
+    # death by having no energy
+    def die(self):
+        if self.energy == 0:
+            self.destroy()
+
+    def destroy(self):
+        self.model: WolfSheepModel
+        self.model.schedule.remove(self)
+        self.model.grid.remove_agent(self)
 
 
 class WolfAgent(WolfSheepAgent):
     def __init__(self, unique_id: int, model: WolfSheepModel, energy: int, gender: bool = False):
         super().__init__(unique_id, model, energy, gender)
+        self.race = 0
 
 
 class SheepAgent(WolfSheepAgent):
     def __init__(self, unique_id: int, model: WolfSheepModel, energy: int, gender: bool = False):
         super().__init__(unique_id, model, energy, gender)
+        self.race = 1
 
 
 class GrassAgent(Agent):
